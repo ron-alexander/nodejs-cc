dca318a14f6364ee083716ee1d997e9db30f7299
Ron Alexander <ron.alexander@computacenter.com>
2018-12-28 15:59:45 +0000

    Add new file

---
+++ server.js
@@ -0,0 +1,18 @@
+// OpenShift Node application
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var eps = require('ejs');
+app.engine('html', require('ejs').renderFile);
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+app.get('/', function (req, res) {
+res.render('index.html');
+});
+// error handling
+app.use(function(err, req, res, next){
+console.error(err.stack);
+res.status(500).send('unexpected exit');
+});
+app.listen(port, ip);
+console.log('Server running on ' + ip + ':' + port);
