401889d999571105a6606f13873841c61a32381c
Ron Alexander <ron.alexander@computacenter.com>
2019-01-02 10:24:01 +0000

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 // app.get('/', function (req, res) {
 // res.render('index.html');
 // });
-// // error handling
+// error handling
 // app.use(function(err, req, res, next){
 // console.error(err.stack);
 // res.status(500).send('Something bad happened!');
